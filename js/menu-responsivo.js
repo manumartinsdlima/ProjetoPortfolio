a8045d9664db13d750899372b078846a0d8fb6d0
ManuMartins05 <106791789+ManuMartins05@users.noreply.github.com>
2024-05-26 20:27:33 -0300

    Implementar menu responsivo
    Implementação do menu responsivo (mobile) e suas respectivas animações

---
+++ js/menu-responsivo.js
@@ -0,0 +1,23 @@
+const menu = document.querySelector(".logo-menu");
+
+const nav = document.querySelector(".nav-menu");
+
+const nav_elemento1 = document.getElementById("nav-elemento1");
+
+const nav_elemento2 = document.getElementById("nav-elemento2");
+
+const nav_elemento3 = document.getElementById("nav-elemento3");
+
+const nav_elemento4 = document.getElementById("nav-elemento4");
+
+menu.addEventListener("click", () => nav.classList.toggle("active"));
+
+menu.addEventListener("click", () => menu.classList.toggle("fechar"));
+
+nav_elemento1.addEventListener("click", () => nav.classList.toggle("active"));
+
+nav_elemento2.addEventListener("click", () => nav.classList.toggle("active"));
+
+nav_elemento3.addEventListener("click", () => nav.classList.toggle("active"));
+
+nav_elemento4.addEventListener("click", () => nav.classList.toggle("active"));
